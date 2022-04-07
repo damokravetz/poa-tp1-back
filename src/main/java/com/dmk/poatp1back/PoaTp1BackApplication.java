c166426205491cc26738f0d6fb8fa479e1e3a1f6
damokravetz <kravetzdamian@gmail.com>
2022-04-07 02:32:42 -0300

    first commit

---
+++ src/main/java/com/dmk/poatp1back/PoaTp1BackApplication.java
@@ -0,0 +1,13 @@
+package com.dmk.poatp1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoaTp1BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoaTp1BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmk/poatp1back/PoaTp1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmk.poatp1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoaTp1BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
